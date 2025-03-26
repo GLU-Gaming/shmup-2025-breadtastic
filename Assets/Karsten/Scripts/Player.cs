1fdd345c7afc335a8e02ab3325a8ad0c75ae9817
karsten kooistra <karstenpfk@gmail.com>
2025-03-26 11:53:41 +0100

    changes

--- Assets/Karsten/Scripts/Player.cs
+++ Assets/Karsten/Scripts/Player.cs
@@ -4,7 +4,9 @@ public class Player : MonoBehaviour
 {
     public int lives = 15; // Aantal levens van de speler
 
-    public void TakeDamage(int damage)
+    private int damage = 1; // Hoeveelheid schade die de speler kan incasseren
+
+    public void TakeDamage()
     {
         lives -= damage; // Verminder het aantal levens met de hoeveelheid schade
         if (lives <= 0)
@@ -12,5 +14,34 @@ public class Player : MonoBehaviour
             // Logica voor wanneer de speler sterft
             Debug.Log("Player is dead!");
         }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log($"EnemyBullet collided with: {collision.gameObject.name}");
+
+        // Controleer of de kogel de speler raakt
+        
+            Debug.Log("EnemyBullet hit the Player");
+
+            // Breng schade toe aan de speler
+        EnemyBullet enemyBullet = collision.GetComponent<EnemyBullet>();
+        if (enemyBullet != null)
+        {
+            damage = enemyBullet.damage;
+            TakeDamage();
+        }
+
+
+        // Breng schade toe aan de speler
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy)
+        {
+            damage = 1; // Verminder het aantal levens van de speler met 1
+            TakeDamage();
+        }
+
+        // Vernietig de vijand
+        Destroy(collision.gameObject);
     }
 }
