5c4a10f6f89c76b26e8cd6baeb5a45f93cf523c1
karsten kooistra <karstenpfk@gmail.com>
2025-04-09 08:58:17 +0200

    .

--- Assets/Karsten/Scripts/ScoreManager.cs
+++ Assets/Karsten/Scripts/ScoreManager.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
@@ -7,6 +8,8 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
     public TextMeshProUGUI scoreText;
     public int score = 0;
+
+    public string bossSceneName = "BossScene"; // Name of the boss scene
 
     void Awake()
     {
@@ -30,10 +33,31 @@ public class ScoreManager : MonoBehaviour
     {
         score += points;
         UpdateScoreText();
+
+        // Check if the score reaches 700
+        if (score >= 700)
+        {
+            TriggerBossFight();
+        }
     }
 
     void UpdateScoreText()
     {
         scoreText.text = "Score: " + score;
+    }
+
+    void TriggerBossFight()
+    {
+        Debug.Log("Boss fight triggered! Loading boss scene...");
+
+        // Load the boss scene
+        if (!string.IsNullOrEmpty(bossSceneName))
+        {   
+            SceneManager.LoadScene(bossSceneName);
+        }
+        else
+        {
+            Debug.LogError("Boss scene name is not set in the ScoreManager.");
+        }
     }
 }
